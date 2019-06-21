9ff1393f5f3c0aaa682984703a848d5bc7a5a5e5
katsshura <emerson.r.alves.a@gmail.com>
2019-06-21 19:35:41 -0300

    refactor(Listener): Implemented database object
    - Implementation of database object on main Listener class

--- RabbitMQ.Robot/RabbitMQ.Robot.Infrastructure/RabbitListener.cs
+++ RabbitMQ.Robot/RabbitMQ.Robot.Infrastructure/RabbitListener.cs
@@ -1,5 +1,7 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using RabbitMQ.Robot.Domain;
+using RabbitMQ.Robot.Infrastructure.DataContent;
 using System;
 using System.Text;
 
@@ -7,6 +9,17 @@ namespace RabbitMQ.Robot.Infrastructure
 {
     public class RabbitListener
     {
+        private readonly SqlServerData db;
+
+        public RabbitListener(SqlServerData db)
+        {
+            this.db = db;
+        }
+
+        #region Receiver
+
+        //Receive message and assigns the correct method
+
         public void ReceiveMessageOnQueue(string route)
         {
             Console.WriteLine("\nRunning Robot Setup...");
@@ -17,31 +30,63 @@ namespace RabbitMQ.Robot.Infrastructure
             {
                 consumer.Received += OnReceivedMessageBrowser;
             }
-            else
+            else if(route == RabbitRoutesUtillity.Purchase_Route)
             {
                 consumer.Received += OnReceivedMessagePurchase;
+            }
+            else if(route == RabbitRoutesUtillity.User_Route)
+            {
+                consumer.Received += OnReceivedMessageUser;
             }
 
             channel.BasicConsume(queue: route, autoAck: true, consumer: consumer);
             Console.WriteLine("\nListening on: " + route);
-            
         }
+        #endregion
 
+        #region Handlers OnReceivedMessage
+
+        /// <summary> Handle the messages from RabbitMQ
+        ///<para>Messages are separated according to their queue. Then json is converted into the respective queue object</para>
+        /// </summary>
+        /// 
         private void OnReceivedMessageBrowser(object consumer, BasicDeliverEventArgs eventArgs)
         {
             string message = DecodeEventArgBody(eventArgs.Body);
-            Console.WriteLine(" [x] Received on Browser{0}", message);
+            BrowserInformation browserObj = BrowserInformation.FromJson(message);
+
         }
 
         private void OnReceivedMessagePurchase(object consumer, BasicDeliverEventArgs eventArgs)
         {
             string message = DecodeEventArgBody(eventArgs.Body);
-            Console.WriteLine(" [x] Received on Purchase {0}", message);
+            Purchase purchaseObj = Purchase.FromJson(message);
         }
+
+        private void OnReceivedMessageUser(object consumer, BasicDeliverEventArgs eventArgs)
+        {
+            string message = DecodeEventArgBody(eventArgs.Body);
+            User userObj = User.FromJson(message);
+        }
+
+        #endregion
 
         private string DecodeEventArgBody(byte[] body)
         {
             return Encoding.UTF8.GetString(body);
+        }
+
+        private void AddObjectIntoDatabase(object obj)
+        {
+            try
+            {
+                db.Add(obj);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
