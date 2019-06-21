6979be8b117b03c2879525c441d335cbac0e2e31
katsshura <emerson.r.alves.a@gmail.com>
2019-06-21 00:22:34 -0300

    feat(infrastructure): Implementation of Rabbit Connection - Implemented RabbitMQ dependency - Implemented method to send message to Rabbit Queue

---
+++ WebApi.Client.Infrastructure/RabbitMQConnection.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+
+namespace WebApi.Client.Infrastructure
+{
+    public sealed class RabbitMQConnection
+    {
+        public static IModel GetRabbitMQChannel(string host, string queue) {
+            var factory = new ConnectionFactory() { HostName = host };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(queue: queue,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+            return channel;
+        }
+    }
+}
