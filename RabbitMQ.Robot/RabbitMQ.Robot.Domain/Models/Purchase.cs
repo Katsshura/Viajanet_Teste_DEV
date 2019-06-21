c288d458b0d2119ff778b372432407ed9ebfd66d
katsshura <emerson.r.alves.a@gmail.com>
2019-06-21 04:56:15 -0300

    feat(Models): Implementation of new domain model

---
+++ RabbitMQ.Robot/RabbitMQ.Robot.Domain/Models/Purchase.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Robot.Domain.Models
+{
+    public class Purchase
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("productId")]
+        public long ProductId { get; set; }
+
+        public static Purchase FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Purchase>(json, JsonConverter.Settings);
+        }
+
+        public static string ToJson(Purchase self)
+        {
+            return JsonConvert.SerializeObject(self, JsonConverter.Settings);
+        }
+
+    }
+}
