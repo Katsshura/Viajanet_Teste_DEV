828735abc4df04c18cdae441fae03339f4b51ca0
katsshura <emerson.r.alves.a@gmail.com>
2019-06-21 19:37:00 -0300

    feat(sql): Added DataContext for Entity Framework Core

---
+++ RabbitMQ.Robot/RabbitMQ.Robot.Infrastructure/DataContent/SqlServerData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Robot.Domain;
+
+namespace RabbitMQ.Robot.Infrastructure.DataContent
+{
+    public class SqlServerData : DbContext
+    {
+        public DbSet<BrowserInformation> BrowserInformations { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
