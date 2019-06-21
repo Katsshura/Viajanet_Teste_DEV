92f02ca1c54211a8fda1b9eb8749d080776840c4
katsshura <emerson.r.alves.a@gmail.com>
2019-06-21 04:28:39 -0300

    feat: Implementation of Robot Initializer

---
+++ RabbitMQ.Robot/RabbitMQ.Robot.Infrastructure/RabbitRoutesUtillity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Robot.Infrastructure
+{
+    public class RabbitRoutesUtillity
+    {
+        public static readonly string Browser_Route = "client_browser_info";
+        public static readonly string Purchase_Route = "client_purchase_info";
+    }
+}
