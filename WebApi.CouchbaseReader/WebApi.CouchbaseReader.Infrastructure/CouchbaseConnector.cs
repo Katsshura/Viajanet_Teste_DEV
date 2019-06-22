b2795b055934a10d18e4ba2407ccb1038a70eee7
katsshura <emerson.r.alves.a@gmail.com>
2019-06-22 19:09:51 -0300

    feat(controllers): implementation of api controllers
    - Implemented UserController
    - Implemented ProductController
    - Implemented BrowserInformationController

--- WebApi.CouchbaseReader/WebApi.CouchbaseReader.Infrastructure/CouchbaseConnector.cs
+++ WebApi.CouchbaseReader/WebApi.CouchbaseReader.Infrastructure/CouchbaseConnector.cs
@@ -26,6 +26,11 @@ namespace WebApi.CouchbaseReader.Infrastructure
             }, new PasswordAuthenticator(User, Password));
 
             bucket = ClusterHelper.GetBucket(BucketName);
+
+            if (bucket != null)
+            {
+                CreateIndexesOnCouchbase();
+            }
         }
 
         public static IBucket GetBucketInstance()
@@ -36,6 +41,29 @@ namespace WebApi.CouchbaseReader.Infrastructure
         public static void CloseCouchbaseConnection()
         {
             ClusterHelper.Close();
+        }
+
+        private static async void CreateIndexesOnCouchbase()
+        {
+            var bucketManager = bucket.CreateManager();
+            await bucketManager.CreateN1qlPrimaryIndexAsync(true);
+            await bucketManager.CreateN1qlIndexAsync("ix_type", true, new string[] { "type" });
+            await bucketManager.CreateN1qlIndexAsync("ix_emai", true, new string[] { "email" });
+            await bucketManager.CreateN1qlIndexAsync("ix_pass", true, new string[] { "pass" });
+            await bucketManager.CreateN1qlIndexAsync("ix_title", true, new string[] { "title" });
+            await bucketManager.CreateN1qlIndexAsync("ix_ip", true, new string[] { "ip" });
+            await bucketManager.CreateN1qlIndexAsync("ix_pageName", true, new string[] { "pageName" });
+            await bucketManager.BuildN1qlDeferredIndexesAsync();
+            bucketManager.WatchN1qlIndexes(
+                new List<string> {
+                "ix_type",
+                "ix_email",
+                "ix_pass",
+                "ix_title",
+                "ix_ip",
+                "ix_pageName",
+                "#primary"
+            }, TimeSpan.FromSeconds(2));
         }
     }
 }
