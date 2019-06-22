91f96bd5f7429fa5767ecf618c0962d7e066f42c
katsshura <emerson.r.alves.a@gmail.com>
2019-06-22 16:43:21 -0300

    feat: Implementation of couchbase connector

--- WebApi.CouchbaseReader/WebApi.CouchbaseReader.Api/Startup.cs
+++ WebApi.CouchbaseReader/WebApi.CouchbaseReader.Api/Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using WebApi.CouchbaseReader.Infrastructure;
 
 namespace WebApi.CouchbaseReader.Api
 {
@@ -25,10 +20,11 @@ namespace WebApi.CouchbaseReader.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            CouchbaseConnector.OpenCouchbaseConnection();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
@@ -36,6 +32,7 @@ namespace WebApi.CouchbaseReader.Api
             }
 
             app.UseMvc();
+            applicationLifetime.ApplicationStopped.Register(() => CouchbaseConnector.CloseCouchbaseConnection());
         }
     }
 }
---
+++ WebApi.CouchbaseReader/WebApi.CouchbaseReader.Infrastructure/CouchbaseConnector.cs
@@ -0,0 +1,41 @@
+﻿using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Configuration.Client;
+using Couchbase.Core;
+using System;
+using System.Collections.Generic;
+
+namespace WebApi.CouchbaseReader.Infrastructure
+{
+    public class CouchbaseConnector
+    {
+        private static readonly string User = "myuser";
+        private static readonly string Password = "123456";
+        private static readonly string BucketName = "ViajanetDB";
+
+        private static IBucket bucket;
+
+        public static void OpenCouchbaseConnection()
+        {
+            ClusterHelper.Initialize(new ClientConfiguration
+            {
+                Servers = new List<Uri> {
+                    new Uri("couchbase://localhost")
+                }
+
+            }, new PasswordAuthenticator(User, Password));
+
+            bucket = ClusterHelper.GetBucket(BucketName);
+        }
+
+        public static IBucket GetBucketInstance()
+        {
+            return bucket;
+        }
+
+        public static void CloseCouchbaseConnection()
+        {
+            ClusterHelper.Close();
+        }
+    }
+}
