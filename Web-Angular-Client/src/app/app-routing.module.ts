33daa02c2fd80536afa77e0716f891bbe2e35de8
katsshura <emerson.r.alves.a@gmail.com>
2019-06-23 04:57:04 -0300

    feat(toastr): implemented toastr

--- Web-Angular-Client/src/app/app-routing.module.ts
+++ Web-Angular-Client/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { SigninComponent } from './user/signin/signin.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/user/signin', pathMatch: 'full'},
-  {path: 'user', component: UserComponent, children: [
+  {path:'home', redirectTo:'/home/signin', pathMatch: 'full'},
+  {path: '', redirectTo: '/home/signin', pathMatch: 'full'},
+  {path: 'home', component: UserComponent, children: [
       {path: 'signup', component: SignupComponent},
       {path: 'signin', component: SigninComponent},
-    ]}
+    ]},
+    {path:'landing', component: LandingPageComponent}
 ];
 
 @NgModule({
--- Web-Angular-Client/src/app/app.module.ts
+++ Web-Angular-Client/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignupComponent } from './user/signup/signup.component';
 import { SigninComponent } from './user/signin/signin.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
