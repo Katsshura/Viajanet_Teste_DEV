bff23572d8650215d021eb01e72a2c26af6da00e
katsshura <emerson.r.alves.a@gmail.com>
2019-06-24 05:58:03 -0300

    feat(js): Implementation of api_service.json to make requests to api

--- Web-Angular-Client/src/assets/js/api_services.js
+++ Web-Angular-Client/src/assets/js/api_services.js
@@ -4,41 +4,31 @@ const HOST_POST_API = 'http://localhost:55472/api/v1/';
 async function getResponse(url, params, callback) {
   let res;
   if (params === null) {
-    res = await $.get(HOST_GET_API + url, callback);
+    res = await $.get(HOST_GET_API + url, callback).catch(c => console.log(c));
   } else {
-    res = await $.get(HOST_GET_API + url, params, callback).catch(c => {
-      //verify if what was caught is an error
-      if (c.status != 200) {
-        //if it is, execute callback with res = null and status of failed
-        callback(null, 'failed')
-      }
-    });
+    res = await $.get(HOST_GET_API + url, params, callback).catch(c => console.log(c));
   }
   return res;
 }
 
-async function postResponse(url, data, callback) {
-  let res;
-  res = await $.post(HOST_POST_API + url, JSON.stringify(data), callback).catch(c => {
-    if((c.status == 200 || c.status == 201) === false){
-      callback(null, "failed");
-      console.log(c);
-    }
+async function postResponse(url, dtr, callback) {
+  $.ajax({
+    type: 'POST',
+    dataType: "json", // expected format for response
+    contentType: "application/json",
+    url: HOST_POST_API+url,
+    data: JSON.stringify(dtr),
+    success: function (dataResponse) {
+      callback(dataResponse);
+    },
+    error: function () {
+      console.log("Error", dtr);
+    },
   });
-  return res;
 }
 
-async function getIp() {
-  let res = await $.getJSON("https://api.ipify.org?format=json");
+//Using free api to get public ip
+async function getIp(callback) {
+  let res = await $.getJSON("https://api.ipify.org?format=json").done(callback);
   return res.ip;
-}
-
-async function getBrowserInformation() {
-  BrowserInformation =
-    {
-      "ip": await getIp(),
-      "pageName": location.pathname.substring(location.pathname.lastIndexOf("/") + 1).toLowerCase(),
-      "name": navigator.appCodeName.toLocaleLowerCase(),
-    }
-  return await BrowserInformation;
 }
